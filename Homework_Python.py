7e922ba05f509818835d68f48a6694e0d1492494
Aleksey <khatuntsev1979@mail.ru>
2024-02-28 19:26:36 +0400

    Homework № 16

--- Homework_Python.py
+++ Homework_Python.py
@@ -343,4 +343,24 @@ from random import randint
 
 # Homework № 15 -----------------------------------------------------------
 
-print(list(filter(lambda i: i == i[::-1], ('madam', 'fire', 'tomato', 'book', 'kiosk', 'mom'))))
+# print(list(filter(lambda i: i == i[::-1], ('madam', 'fire', 'tomato', 'book', 'kiosk', 'mom'))))
+
+# Homework № 16 -----------------------------------------------------------
+
+def avg(fn):
+    def wrap(*arg):
+        a = ""
+        for i in arg:
+            a += str(i) + ", "  # "2, 3, 3, 4, "
+        print("Среднее арифметическое:", a[:-2], "=", fn(*arg) / len(arg))
+
+    return wrap
+
+
+@avg
+def summa(*args):  # (2, 3, 3, 4)
+    print("Сумма чисел:", ", ".join(map(str, args)), "=", sum(args))
+    return sum(args)
+
+
+summa(2, 3, 3, 4)
